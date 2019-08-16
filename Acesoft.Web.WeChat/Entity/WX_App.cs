cffd9d5bbb90a3202124e16fcbc82128eac875b4
Alex-ling <mingzhao-ling@163.com>
2019-08-16 14:16:30 +0800

    更名完成

---
+++ Acesoft.Web.WeChat/Entity/WX_App.cs
@@ -0,0 +1,18 @@
+using Acesoft.Data;
+using Dapper.Contrib.Extensions;
+
+namespace Acesoft.Web.WeChat.Entity
+{
+    [Table("wx_app")]
+    public class Wx_App : EntityBase
+    {
+        public string Name { get; set; }
+        public string AppId { get; set; }
+        public string AppSecret { get; set; }
+        public string Token { get; set; }
+        public string AesKey { get; set; }
+        public WxType Type { get; set; }
+        public string Secret { get; set; }
+        public string Remark { get; set; }
+    }
+}
